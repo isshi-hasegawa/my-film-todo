ad1de4c112343000eaf013e8e7d00dc0b4424a83
Masashi Hasegawa <53898556+hiroshima820@users.noreply.github.com>
2022-08-23 07:59:21 +0900

    ESLint導入

--- next.config.js
+++ next.config.js
@@ -2,6 +2,10 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+  eslint: {
+    dirs: ['src/components/', 'src/lib/', 'src/pages/'],
+    ignoreDuringBuilds: true,
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
