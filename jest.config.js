45a2eaf8aff80d90c7396c6b47f46b09d20e9b9b
Masashi Hasegawa <53898556+hiroshima820@users.noreply.github.com>
2022-08-26 12:23:20 +0900

    Jestのルートをsrcに設定

--- jest.config.js
+++ jest.config.js
@@ -6,6 +6,7 @@ const createJestConfig = nextJest({
 
 const customJestConfig = {
   setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+  roots: ['<rootDir>/src/'],
   moduleDirectories: ['node_modules', '<rootDir>/'],
   testEnvironment: 'jest-environment-jsdom',
 };
