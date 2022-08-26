a6d3f0d711ad7d1b72c00a8336d5814d2ba496bc
Masashi Hasegawa <53898556+hiroshima820@users.noreply.github.com>
2022-08-26 12:06:18 +0900

    CIをJestのみに修正

--- jest.config.js
+++ jest.config.js
@@ -5,6 +5,7 @@ const createJestConfig = nextJest({
 });
 
 const customJestConfig = {
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
   moduleDirectories: ['node_modules', '<rootDir>/'],
   testEnvironment: 'jest-environment-jsdom',
 };
--- src/components/Sample.tsx
+++
@@ -1,8 +0,0 @@
-export const Sample = () => {
-  return (
-    <main>
-      <h1>Nextjs+Jestのサンプルサプリ</h1>
-      <p>設定がすごく楽になりました。</p>
-    </main>
-  );
-};
