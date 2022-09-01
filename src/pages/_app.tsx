71467624d1a056df772f2aad951d02bc6e0ea794
Masashi Hasegawa <53898556+hiroshima820@users.noreply.github.com>
2022-09-01 17:35:31 +0900

    ヘッダーの雛形を作成

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,21 +1,12 @@
 import { SessionProvider } from "next-auth/react";
-import { ChakraProvider, Grid } from "@chakra-ui/react";
+import { ChakraProvider } from "@chakra-ui/react";
 import type { AppProps } from "next/app";
 
 function MyApp({ Component, pageProps: { session, ...pageProps } }: AppProps) {
   return (
     <SessionProvider session={session}>
       <ChakraProvider>
-        <Grid
-          sx={{
-            h: "100vh",
-            placeItems: "center",
-            px: "5rem",
-            textAlign: "center",
-          }}
-        >
-          <Component {...pageProps} />
-        </Grid>
+        <Component {...pageProps} />
       </ChakraProvider>
     </SessionProvider>
   );
