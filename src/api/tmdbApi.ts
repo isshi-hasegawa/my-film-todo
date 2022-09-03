03c855606eabd017f948734966fd247718b153aa
Masashi Hasegawa <53898556+hiroshima820@users.noreply.github.com>
2022-09-03 17:18:14 +0900

    映画の情報取得のAPIの雛形を作成

---
+++ src/api/tmdbApi.ts
@@ -0,0 +1,27 @@
+import { MovieDb } from "moviedb-promise";
+import {
+  MovieImagesResponse,
+  MovieResponse,
+  MovieResultsResponse,
+  WatchProviderResponse,
+} from "moviedb-promise/dist/request-types";
+
+const moviedb = new MovieDb(process.env.TMDB_API_KEY as string);
+
+export async function getMovieData(id: number): Promise<MovieResponse> {
+  return await moviedb.movieInfo({ id });
+}
+
+export async function getWatchProviders(
+  id: number
+): Promise<WatchProviderResponse> {
+  return await moviedb.movieWatchProviders({ id });
+}
+
+export async function getMovieImages(id: number): Promise<MovieImagesResponse> {
+  return await moviedb.movieImages({ id });
+}
+
+export async function searchMovie(name: string): Promise<MovieResultsResponse> {
+  return await moviedb.searchMovie({ query: name });
+}
