216a8bf93c590dc65a829abe8a594acead396032
Masashi Hasegawa <53898556+hiroshima820@users.noreply.github.com>
2022-09-05 17:46:07 +0900

    検索画面のコンポーネント名を変更

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,16 @@
 import type { NextPage } from "next";
 import { useSession } from "next-auth/react";
-import { Grid, Heading, Spinner } from "@chakra-ui/react";
+import { Button, Grid, Heading, Spacer, Spinner } from "@chakra-ui/react";
 import SignIn from "./auth/signin";
 import Header from "src/components/Header";
 import { Tasks } from "src/components/Tasks";
 import { useState } from "react";
+import Search from "src/components/Search";
 
 const Home: NextPage = () => {
   const { data: session, status } = useSession();
   const [selectedTaskListId, setSelectedTaskListId] = useState<string>("");
+  const [isShowSearchMovies, setIsShowSearchMovies] = useState<boolean>(false);
 
   if (status === "loading")
     return (
@@ -37,17 +39,40 @@ const Home: NextPage = () => {
           <SignIn />
         </Grid>
       )}
-      {session && (
+      {session && !isShowSearchMovies && (
         <>
-          <Header setSelectedTaskListId={setSelectedTaskListId} />
+          <Header
+            setSelectedTaskListId={setSelectedTaskListId}
+            setIsShowSearchMovies={setIsShowSearchMovies}
+          />
           <Grid
             sx={{
               placeItems: "center",
               px: "5rem",
             }}
-            paddingTop="64px"
+            paddingTop="72px"
           >
+            <Button onClick={() => setIsShowSearchMovies(true)}>
+              タスクを登録する
+            </Button>
             <Tasks selectedTaskListId={selectedTaskListId} />
+          </Grid>
+        </>
+      )}
+      {session && isShowSearchMovies && (
+        <>
+          <Header
+            setSelectedTaskListId={setSelectedTaskListId}
+            setIsShowSearchMovies={setIsShowSearchMovies}
+          />
+          <Grid
+            sx={{
+              placeItems: "center",
+              px: "5rem",
+            }}
+            paddingTop="72px"
+          >
+            <Search />
           </Grid>
         </>
       )}
