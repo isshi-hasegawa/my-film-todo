a3d546b627301bfb82498a6cf4eaeec4260f146e
Masashi Hasegawa <53898556+hiroshima820@users.noreply.github.com>
2022-09-07 01:36:54 +0900

    WatchProvider型をexport

--- src/api/tmdbApi.ts
+++ src/api/tmdbApi.ts
@@ -6,16 +6,17 @@ import {
   WatchProviderResponse,
 } from "moviedb-promise/dist/request-types";
 
+export type WatchProvider = {
+  display_priority?: number;
+  logo_path?: string;
+  provider_id?: number;
+  provider_name?: string;
+};
+
 const moviedb = new MovieDb(process.env.NEXT_PUBLIC_TMDB_API_KEY as string);
 
 export async function getMovieData(id: number): Promise<MovieResponse> {
   return await moviedb.movieInfo({ id });
-}
-
-export async function getWatchProviders(
-  id: number
-): Promise<WatchProviderResponse> {
-  return await moviedb.movieWatchProviders({ id });
 }
 
 export async function getMovieImages(id: number): Promise<MovieImagesResponse> {
@@ -30,4 +31,10 @@ export const searchMovie = async (
     include_adult: false,
     language: "ja-JP",
   });
+};
+
+export const getWatchProviders = async (
+  id: number
+): Promise<WatchProviderResponse> => {
+  return await moviedb.movieWatchProviders({ id });
 };
