1f7c10eef56a27e4551714a150b51ee182d2b201
Masashi Hasegawa <53898556+hiroshima820@users.noreply.github.com>
2022-09-08 12:03:11 +0900

    タスクの全件取得を実装

--- src/api/tasksApi.ts
+++ src/api/tasksApi.ts
@@ -2,22 +2,13 @@ import { api } from 'src/utils/api'
 import type {
   Task,
   TasksResponse,
-  GetTasksResponse,
   GetTasksParams,
   CreateTaskParam,
 } from 'src/types/tasks'
 
-// type GetTasksNextPageParams = {
-//   taskListId: string
-//   nextPageToken?: string
-// }
-
-export const getTasks = async (
-  params: GetTasksParams,
-  token: string
-): Promise<GetTasksResponse> => {
+export const getTasks = async (params: GetTasksParams, token: string) => {
   const response = await api.get<TasksResponse>(
-    `https://tasks.googleapis.com/tasks/v1/lists/${params.taskListId}/tasks?maxResults=100`,
+    `https://tasks.googleapis.com/tasks/v1/lists/${params.taskListId}/tasks?maxResults=100&pageToken=${params.nextPageToken}`,
     {
       params: {
         ...params,
@@ -27,30 +18,8 @@ export const getTasks = async (
       },
     }
   )
-  return {
-    items: response.data.items,
-    nextPageToken: response.data.nextPageToken,
-  }
+  return response
 }
-
-// export const getTasksNextPage = async (
-//   params: GetTasksNextPageParams,
-//   token: string
-// ) => {
-//   const response = await api.get<TasksResponse>(
-//     `https://tasks.googleapis.com/tasks/v1/lists/${params.taskListId}/tasks?maxResults=100?pageToken=${params.nextPageToken}`,
-//     {
-//       params: {
-//         ...params,
-//       },
-//       headers: {
-//         Authorization: `Bearer ${token}`,
-//       },
-//     }
-//   );
-
-//   return response;
-// };
 
 export const createTask = async (
   params: CreateTaskParam,
