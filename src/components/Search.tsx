4c6924e5832413ea0acbcfcfa548fba9775d76db
Masashi Hasegawa <53898556+hiroshima820@users.noreply.github.com>
2022-09-10 20:34:09 +0900

    タスクの作成をreact-queryにリプレイス

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -11,7 +11,7 @@ import {
   VStack,
 } from '@chakra-ui/react'
 import { MovieResult } from 'moviedb-promise/dist/request-types'
-import { useEffect, useState } from 'react'
+import { useState } from 'react'
 import { getMovieData, searchMovie } from 'src/api/tmdbApi'
 import { FiPlusCircle } from 'react-icons/fi'
 import WatchProviders from 'src/components/WatchProviders'
@@ -51,30 +51,35 @@ const Search = ({ taskListId }: Props) => {
     fetchSearchResults
   )
 
-  const handleCreateTask = (id: number) => {
-    ;(async () => {
-      const response = await getMovieData(id, 'watch/providers')
-      const title = response.title
-      let notes: string = ''
-      response['watch/providers']?.results?.JP?.flatrate?.map((provider) => {
-        if (provider.provider_name === 'Netflix')
-          notes = notes.concat('Netflix', ' ')
-        if (provider.provider_name === 'Amazon Prime Video')
-          notes = notes.concat('Amazon Prime Video', ' ')
-        if (provider.provider_name === 'Disney Plus')
-          notes = notes.concat('Disney+', ' ')
-      })
-      notes = notes.concat(`${response.runtime}分`)
+  const queryClient = useQueryClient()
 
-      await createTask(
-        {
-          taskListId,
-          title,
-          notes,
-        },
-        token
-      )
-    })()
+  const { mutate: createTaskMutate, isLoading } = useMutation(
+    (resultId: number) => mutateCreateTask(resultId),
+    { onSuccess: () => queryClient.invalidateQueries(['tasks']) }
+  )
+
+  const mutateCreateTask = async (id: number) => {
+    const response = await getMovieData(id, 'watch/providers')
+    const title = response.title
+    let notes: string = ''
+    response['watch/providers']?.results?.JP?.flatrate?.map((provider) => {
+      if (provider.provider_name === 'Netflix')
+        notes = notes.concat('Netflix', ' ')
+      if (provider.provider_name === 'Amazon Prime Video')
+        notes = notes.concat('Amazon Prime Video', ' ')
+      if (provider.provider_name === 'Disney Plus')
+        notes = notes.concat('Disney+', ' ')
+    })
+    notes = notes.concat(`${response.runtime}分`)
+
+    await createTask(
+      {
+        taskListId,
+        title,
+        notes,
+      },
+      token
+    )
   }
 
   return (
@@ -92,33 +97,37 @@ const Search = ({ taskListId }: Props) => {
           <Spinner size="xl" placeItems="center" />
         ) : (
           <VStack {...vStackProps}>
-            {searchResults?.map((result) => (
-              <HStack
-                key={result.id}
-                onClick={() => handleCreateTask(result.id!)}
-              >
-                <IconButton
-                  icon={<FiPlusCircle />}
-                  isRound
-                  aria-label="Add Button"
-                />
-                {result.poster_path && (
-                  <Image
-                    src={`https://image.tmdb.org/t/p/original/${result.poster_path}`}
-                    alt="poster"
-                    width={{ base: '45px', sm: '60px', md: '150px' }}
-                    height={{ base: '63px', sm: '84px', md: '210px' }}
+            {isLoading ? (
+              <Spinner size="xl" />
+            ) : (
+              searchResults?.map((result) => (
+                <HStack
+                  key={result.id}
+                  onClick={() => createTaskMutate(result.id!)}
+                >
+                  <IconButton
+                    icon={<FiPlusCircle />}
+                    isRound
+                    aria-label="Add Button"
                   />
-                )}
-                <Stack>
-                  <Text>{result.title}</Text>
-                  <Text fontSize="sm" color="gray.600">
-                    {result.release_date?.substring(0, 4)}
-                  </Text>
-                  <WatchProviders id={result.id!} />
-                </Stack>
-              </HStack>
-            ))}
+                  {result.poster_path && (
+                    <Image
+                      src={`https://image.tmdb.org/t/p/original/${result.poster_path}`}
+                      alt="poster"
+                      width={{ base: '45px', sm: '60px', md: '150px' }}
+                      height={{ base: '63px', sm: '84px', md: '210px' }}
+                    />
+                  )}
+                  <Stack>
+                    <Text>{result.title}</Text>
+                    <Text fontSize="sm" color="gray.600">
+                      {result.release_date?.substring(0, 4)}
+                    </Text>
+                    <WatchProviders id={result.id!} />
+                  </Stack>
+                </HStack>
+              ))
+            )}
           </VStack>
         )}
       </Grid>
