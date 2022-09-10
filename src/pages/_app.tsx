4cf4e14ac5d40d70f0206fff43a35e0fc3fd0e28
Masashi Hasegawa <53898556+hiroshima820@users.noreply.github.com>
2022-09-10 13:33:54 +0900

    プロバイダーを用意

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,18 @@
-import { SessionProvider } from 'next-auth/react'
-import { ChakraProvider } from '@chakra-ui/react'
 import type { AppProps } from 'next/app'
+import { SessionProvider } from 'next-auth/react'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { ChakraProvider } from '@chakra-ui/react'
 
 function MyApp({ Component, pageProps: { session, ...pageProps } }: AppProps) {
+  const queryClient = new QueryClient()
+
   return (
     <SessionProvider session={session}>
-      <ChakraProvider>
-        <Component {...pageProps} />
-      </ChakraProvider>
+      <QueryClientProvider client={queryClient}>
+        <ChakraProvider>
+          <Component {...pageProps} />
+        </ChakraProvider>
+      </QueryClientProvider>
     </SessionProvider>
   )
 }
