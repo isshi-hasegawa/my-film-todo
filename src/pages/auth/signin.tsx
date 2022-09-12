cfd23a7d54b8af9f04527eb615d0503c00f9e572
Masashi Hasegawa <53898556+hiroshima820@users.noreply.github.com>
2022-09-13 04:24:46 +0900

    ログインボタンを修正

--- src/pages/auth/signin.tsx
+++ src/pages/auth/signin.tsx
@@ -1,31 +1,19 @@
-import { Box, Button, VStack } from '@chakra-ui/react'
+import { Button, Center, Text } from '@chakra-ui/react'
 import { signIn } from 'next-auth/react'
-import { BsGoogle } from 'react-icons/bs'
-
-const providers = [
-  {
-    name: 'google',
-    Icon: BsGoogle,
-  },
-]
+import { FcGoogle } from 'react-icons/fc'
 
 const SignIn = () => {
   return (
-    <Box>
-      <VStack>
-        {providers.map(({ name, Icon }) => (
-          <Button
-            key={name}
-            leftIcon={<Icon />}
-            onClick={() => signIn(name)}
-            textTransform="uppercase"
-            w="100%"
-          >
-            {name} でログイン
-          </Button>
-        ))}
-      </VStack>
-    </Box>
+    <Button
+      w={'full'}
+      variant={'outline'}
+      leftIcon={<FcGoogle />}
+      onClick={() => signIn('google')}
+    >
+      <Center>
+        <Text>Sign in with Google</Text>
+      </Center>
+    </Button>
   )
 }
 
