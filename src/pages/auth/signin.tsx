c88d4329f1050cb673e764e439a7583d89463f90
Masashi Hasegawa <53898556+hiroshima820@users.noreply.github.com>
2022-10-12 15:52:59 +0900

    一時的にログインボタンを置き換え

--- src/pages/auth/signin.tsx
+++ src/pages/auth/signin.tsx
@@ -1,18 +1,26 @@
 import { Button, Center, Text } from '@chakra-ui/react'
 import { signIn } from 'next-auth/react'
 import { FcGoogle } from 'react-icons/fc'
+import Image from 'next/image'
 
 const SignIn = () => {
   return (
-    <Button
-      variant={'outline'}
-      leftIcon={<FcGoogle />}
+    // <Button
+    //   variant={'outline'}
+    //   leftIcon={<FcGoogle />}
+    //   onClick={() => signIn('google')}
+    // >
+    //   <Center>
+    //     <Text>Googleでログインする</Text>
+    //   </Center>
+    // </Button>
+    <Image
+      src="/btn_google_signin_light_normal_web.png"
+      alt="signin_button"
+      width={191}
+      height={46}
       onClick={() => signIn('google')}
-    >
-      <Center>
-        <Text>Googleでログインする</Text>
-      </Center>
-    </Button>
+    />
   )
 }
 
