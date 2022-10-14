5f8167eca4ac5ebe632658ea84302bbaf6f384d2
Masashi Hasegawa <53898556+hiroshima820@users.noreply.github.com>
2022-10-14 17:48:20 +0900

    ImageコンポーネントをChakra UIのものに変更

--- src/pages/auth/signin.tsx
+++ src/pages/auth/signin.tsx
@@ -1,7 +1,6 @@
-import { Button, Center, Text } from '@chakra-ui/react'
+import { Button, Center, Text, Image } from '@chakra-ui/react'
 import { signIn } from 'next-auth/react'
 import { FcGoogle } from 'react-icons/fc'
-import Image from 'next/image'
 
 const SignIn = () => {
   return (
@@ -14,11 +13,10 @@ const SignIn = () => {
     //     <Text>Googleでログインする</Text>
     //   </Center>
     // </Button>
+
     <Image
       src="/btn_google_signin_light_normal_web.png"
-      alt="signin_button"
-      width={191}
-      height={46}
+      alt="login_button"
       onClick={() => signIn('google')}
     />
   )
