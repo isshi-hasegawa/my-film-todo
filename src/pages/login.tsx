57f86fa0293890a565532438012b7c0efaacd018
Masashi Hasegawa <53898556+hiroshima820@users.noreply.github.com>
2022-10-17 17:41:50 +0900

    loginを修正

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -2,17 +2,22 @@ import { Button, Center, Text, Image, Flex } from '@chakra-ui/react'
 import { signIn } from 'next-auth/react'
 import { FcGoogle } from 'react-icons/fc'
 import Footer from 'src/components/footer'
+import { useRouter } from 'next/router'
 
 const LogIn = () => {
+  const router = useRouter()
+
   return (
     <Flex direction="column" minH="100vh" placeItems="center">
       <Text>ログインしてください</Text>
       <Image
         src="/btn_google_signin_light_normal_web.png"
         alt="login_button"
-        onClick={() => signIn('google')}
+        onClick={() => {
+          signIn('google')
+          router.push('login')
+        }}
       />
-      <Footer />
     </Flex>
   )
 }
