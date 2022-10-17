e36bf842db130318dd84182fff6c5824d76d38f7
Masashi Hasegawa <53898556+hiroshima820@users.noreply.github.com>
2022-10-17 14:11:41 +0900

    Aboutにデモ画面を追加

--- src/components/about/index.tsx
+++ src/components/about/index.tsx
@@ -1,4 +1,12 @@
-import { Heading, Image, Text, VStack, Link } from '@chakra-ui/react'
+import {
+  Heading,
+  Image,
+  Text,
+  VStack,
+  Link,
+  UnorderedList,
+  ListItem,
+} from '@chakra-ui/react'
 import SignIn from 'src/pages/auth/signin'
 
 const About = () => {
@@ -10,8 +18,10 @@ const About = () => {
         </Heading>
       </VStack>
       <br />
+      <Text as="b">どんなサービス？</Text>
+      <br />
       <Text>
-        動画配信サービスで大量に映画鑑賞する人のためのタスク管理ツール
+        動画配信サービスで大量に映画鑑賞する人のためのタスク管理ツールです
       </Text>
       <br />
       <Text>
@@ -20,26 +30,41 @@ const About = () => {
       <br />
       <SignIn />
       <Text fontSize="sm">ご利用にはGoogleアカウントが必要です</Text>
+      <Text fontSize="sm">
+        このサービスは、あなたのGoogleアカウントに紐づく
+        <Link
+          color="teal.500"
+          href="https://support.google.com/tasks/answer/7675772?co=GENIE.Platform%3DDesktop&hl=ja"
+        >
+          Google Todoリスト
+        </Link>
+        にタスクを追加したり、更新したり、削除する操作を行います
+      </Text>
       <br />
-      <VStack bgColor="ivory">
+      <VStack bgColor="gray.300" px="50px">
         <br />
         <Image src="/demo1.png" alt="demo1" htmlWidth={450} htmlHeight={350} />
         <Text>映画を検索することができます</Text>
-        <Text>気に入った映画をクリックすると...</Text>
+        <Text>各サービスのアイコンは</Text>
+        <UnorderedList>
+          <ListItem>
+            Netflix、Amazon Prime Video、Disney+で見放題配信中
+          </ListItem>
+          <ListItem>Apple TVで購入可能</ListItem>
+        </UnorderedList>
+        <Text>であるということを示しています</Text>
+        <br />
+        <Text>観たい映画をクリックすると...</Text>
         <br />
         <Image src="/demo2.png" alt="demo2" htmlWidth={450} htmlHeight={350} />
         <Text>映画をタスクとして登録することができます</Text>
-        <br />
         <Text>
-          （登録したタスクは
-          <Link
-            color="teal.500"
-            href="https://support.google.com/tasks/answer/7675772?co=GENIE.Platform%3DDesktop&hl=ja"
-          >
-            Google Todoリスト
-          </Link>
-          からも確認できます）
+          登録されたタスクの詳細には、見放題中の動画配信サービスの名前と上映時間が保存されます
         </Text>
+        <br />
+        <Image src="/demo3.png" alt="demo3" htmlWidth={450} htmlHeight={350} />
+        <Text>タスクには期限を設定することができます</Text>
+        <Text>タスクの完了やタスク自体の削除も行うことができます</Text>
         <br />
       </VStack>
       <br />
