1f65ccfbd1ff27c7b0cab0e3d12611bb7440ed05
Masashi Hasegawa <53898556+hiroshima820@users.noreply.github.com>
2022-10-23 03:45:48 +0900

    useCallbackを追加

--- src/hooks/useCustomToast.ts
+++ src/hooks/useCustomToast.ts
@@ -1,11 +1,15 @@
 import { useToast } from '@chakra-ui/react'
+import { useCallback } from 'react'
 
 export const useCustomToast = () => {
   const toast = useToast()
-  const customToast = (
-    title: string,
-    status: 'info' | 'warning' | 'success' | 'error' | 'loading' | undefined
-  ) => toast({ title, status, duration: 3000, position: 'bottom-left' })
 
+  const customToast = useCallback(
+    (
+      title: string,
+      status: 'info' | 'warning' | 'success' | 'error' | 'loading' | undefined
+    ) => toast({ title, status, duration: 3000, position: 'bottom-left' }),
+    [toast]
+  )
   return customToast
 }
